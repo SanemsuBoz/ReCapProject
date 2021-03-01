2b9204248db9689c810f69349f917f2b5b8969f6
SanemsuBoz <72025724+SanemsuBoz@users.noreply.github.com>
2021-03-01 23:03:01 +0300

    Add files via upload

---
+++ DataAccess/Concrete/EntityFramework/EfBrandDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfBrandDal : IBrandDal
+    {
+        public void Add(Brand entity)
+        {
+            using (ReCapDemoDatabaseContext context = new ReCapDemoDatabaseContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Brand entity)
+        {
+            using (ReCapDemoDatabaseContext context = new ReCapDemoDatabaseContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Brand Get(Expression<Func<Brand, bool>> filter)
+        {
+            using (ReCapDemoDatabaseContext context = new ReCapDemoDatabaseContext())
+            {
+                return context.Set<Brand>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Brand> GetAll(Expression<Func<Brand, bool>> filter = null)
+        {
+            using (ReCapDemoDatabaseContext context = new ReCapDemoDatabaseContext())
+            {
+                return filter == null ? context.Set<Brand>().ToList() : context.Set<Brand>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Brand entity)
+        {
+            using (ReCapDemoDatabaseContext context = new ReCapDemoDatabaseContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
